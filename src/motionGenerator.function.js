928be773208c499cbe015b0f4c0c511138ccad77
Wonil Suh <wonilsuh@wonils-mbp.usny.ibm.com>
2017-09-27 12:53:20 -0400

    - fade updates

--- src/motionGenerator.function.js
+++ src/motionGenerator.function.js
@@ -40,19 +40,17 @@
  */
 import "babel-polyfill";
 const motionGenerator = (distance, size = 20, mode = 'move', version = 7, params = {}) => {
-	// console.log('npm:motionCalc:motionGenerator...', params.sizeFactorAdjuster);
+	// console.log('motionCalc:motionGenerator...', size, params);
+
+	params.durationMultiplier = params.durationMultiplier || 1;
 
 	let ret;
 
 	switch(version){
 
-		default:
 		case 6:{
 			const 
-				MODES = ['move', 'scale', 'fade'],
-				SIZE_BASE = 20,
-				M_MIN = 80,
-				N_MIN = 110
+				SIZE_BASE = 20
 			;
 			
 			let 
@@ -64,104 +62,62 @@ const motionGenerator = (distance, size = 20, mode = 'move', version = 7, params
 			size = parseFloat(size);
 			params.sizeFactorAdjuster = parseFloat(params.sizeFactorAdjuster);
 
-			ret = {
-				'meta':{
-					version,
-					params
-				},
-				'input':{
-					distance,
-					size,
-					sizeFactorAdjuster:params.sizeFactorAdjuster
-				},
-				'move':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+			switch(mode){
+
+				case 'fade':{
+					ret = {
+						natural:{
+							curves:{
+								easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
+								easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+							},
+							duration:Math.max(6, 0.01 *distance +5.4285714284) /60 *1000 /sizeFactorNatural *params.durationMultiplier
 						},
-						speed:3 *distance +1200 *sizeFactorNatural
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.2, 0, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						},
-						speed:5 *distance +2500 *sizeFactorMechanical
-					},
-					speedUnit:'px/s',
-					durationUnit:'ms'
-				},
-				'scale':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
-						},
-						speed:3 *distance +1200 *sizeFactorNatural
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.2, 0, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						},
-						speed:5 *distance +2500 *sizeFactorMechanical
-					},
-					speedUnit:'px/s',
-					durationUnit:'ms'
-				},
-				'fade':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
-						},
-						duration:Math.max(6, 0.01 *distance +5.4285714284) / 60 * 1000 /sizeFactorNatural
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.2, 0, 1, 0.9)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						},
-						duration:Math.max(6, 0.005357142857 *distance +3.257142857) /sizeFactorMechanical
-					}
+						mechanical:{
+							curves:{
+								easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
+								easeIn:'cubic-bezier(0.2, 0, 1, 0.9)',
+								easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
+							},
+							duration:Math.max(6, 0.005357142857 *distance +3.257142857) /60 *1000 /sizeFactorMechanical *params.durationMultiplier
+						}
+					};
+					break;
 				}
-			};
 
-			ret.move.natural.duration = Math.max(N_MIN, distance /ret.move.natural.speed *1000);
-			ret.move.mechanical.duration = Math.max(M_MIN, distance /ret.move.mechanical.speed *1000);
-			ret.scale.natural.duration = Math.max(N_MIN, distance /ret.scale.natural.speed *1000);
-			ret.scale.mechanical.duration = Math.max(M_MIN, distance /ret.scale.mechanical.speed *1000);
-			ret.fade.natural.speed = distance / ret.fade.natural.duration *1000;
-			ret.fade.mechanical.speed = distance / ret.fade.mechanical.duration *1000;
-
-			// if(
-			// 	mode != null
-			// 	&& MODES.find( entry => entry === mode) != null
-			// ) ret = {
-			// 	meta:{
-			// 		...ret.meta,
-			// 		mode
-			// 	},
-			// 	input:ret.input,
-			// 	...ret[mode]
-			// }
+				case 'move':
+				case 'scale':
+				default:{
+					ret = {
+						natural:{
+							curves:{
+								easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
+								easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+							},
+							duration:Math.max(110, distance /(3 *distance +1200 *sizeFactorNatural) *1000) *params.durationMultiplier
+						},
+						mechanical:{
+							curves:{
+								easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
+								easeIn:'cubic-bezier(0.2, 0, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
+							},
+							duration:Math.max(80, distance /(5 *distance +2500 *sizeFactorMechanical) *1000) *params.durationMultiplier
+						}
+					};
+					break;
+				}
+			}
 			break;
 		}
 
+		default:
 		case 7:{
 
 			const 
-				MODES = ['move', 'scale', 'fade'],
-				SIZE_BASE = 20,
-				M_MIN = 100,
-				N_MIN = 110
+				SIZE_BASE = 20
 			;
 			
 			let 
@@ -173,116 +129,93 @@ const motionGenerator = (distance, size = 20, mode = 'move', version = 7, params
 			size = parseFloat(size);
 			params.sizeFactorAdjuster = parseFloat(params.sizeFactorAdjuster);
 
-			ret = {
-				'meta':{
-					version,
-					params
-				},
-				'input':{
-					distance,
-					size,
-					sizeFactorAdjuster:params.sizeFactorAdjuster
-				},
-				'move':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
-						}
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.2, 0.1, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						}
-					},
-					speedUnit:'px/s',
-					durationUnit:'ms'
-				},
-				'scale':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
-						}
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.4, 0.1, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						}
-					},
-					speedUnit:'px/s',
-					durationUnit:'ms'
-				},
-				'fade':{
-					natural:{
-						curves:{
-							easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
-							easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
-							easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+			switch(mode){
+
+				case 'fade':{
+
+					let adjustedSize = size / SIZE_BASE *100;
+
+					ret = {
+						natural:{
+							curves:{
+								easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
+								easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+							},
+							duration:Math.max(
+								0.01 *adjustedSize +130,
+								0
+							) *params.durationMultiplier
 						},
-						duration:Math.max(6, 0.01 *distance +5.4285714284) / 60 * 1000 /sizeFactorNatural
-					},
-					mechanical:{
-						curves:{
-							easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
-							easeIn:'cubic-bezier(0.2, 0, 1, 0.9)',
-							easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
-						},
-						duration:Math.max(6, 0.005357142857 *distance +3.257142857) /sizeFactorMechanical
+						mechanical:{
+							curves:{
+								easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
+								easeIn:'cubic-bezier(0.2, 0, 1, 0.9)',
+								easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
+							},
+							duration:Math.max(
+								0.002 *adjustedSize +80,
+								0
+							) *params.durationMultiplier
+						}
 					}
+					break;
 				}
-			};
 
-			// generate duration and speed for move
-			ret.move.natural.duration = Math.max(
-				Math.min(0.1 *distance +112, 142),
-				distance /(3 *distance +1200 *sizeFactorNatural) *1000
-			);
-			// ret.move.natural.speed = distance /ret.move.natural.duration;
-
-			ret.move.mechanical.duration = Math.max(
-				Math.min(0.03 *distance +95, 140),
-				distance /(5 *distance +2500 *sizeFactorMechanical) *1000
-			);
-			// ret.move.natural.speed = distance /ret.move.mechanical.duration;
-
-			// copy move durationa and speed over to scale
-			ret.scale.natural.speed = ret.scale.natural.speed;
-			ret.scale.natural.duration = ret.scale.natural.duration;
-			ret.scale.mechanical.speed = ret.scale.mechanical.speed;
-			ret.scale.mechanical.duration = ret.scale.mechanical.duration;
-
-			ret.fade.natural.speed = distance / ret.fade.natural.duration *1000;
-			ret.fade.mechanical.speed = distance / ret.fade.mechanical.duration *1000;
-
-			// if(
-			// 	mode != null
-			// 	&& MODES.find( entry => entry === mode) != null
-			// ) ret = {
-			// 	meta:{
-			// 		...ret.meta,
-			// 		mode
-			// 	},
-			// 	input:ret.input,
-			// 	...ret[mode]
-			// };
+				case 'move':
+				case 'scale':
+				default:{
+					ret = {
+						natural:{
+							curves:{
+								easeInOut:'cubic-bezier(0.4, 0.14, 0.3, 1)',
+								easeIn:'cubic-bezier(0.4, 0.14, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.3, 1)'
+							},
+							duration:Math.max(
+								Math.min(0.1 *distance +112, 142),
+								distance /(3 *distance +1200 *sizeFactorNatural) *1000
+							) *params.durationMultiplier
+						},
+						mechanical:{
+							curves:{
+								easeInOut:'cubic-bezier(0.2, 0.2, 0.38, 0.9)',
+								easeIn:'cubic-bezier(0.2, 0.1, 1, 1)',
+								easeOut:'cubic-bezier(0, 0, 0.38, 0.9)'
+							},
+							duration:Math.max(
+								Math.min(0.03 *distance +95, 140),
+								distance /(5 *distance +2500 *sizeFactorMechanical) *1000
+							) *params.durationMultiplier
+						}
+					};
+					break;
+				}
+			}
 		}
 	}
 
-	return {
-		meta:{
-			...ret.meta,
-			mode
+	ret = {
+		...ret,
+		'meta':{
+			version,
+			params,
+			units:{
+				duration:'ms',
+				distance:'px'
+			}
 		},
-		input:ret.input,
-		...ret[mode]
+		'input':{
+			mode,
+			distance,
+			size,
+			sizeFactorAdjuster:params.sizeFactorAdjuster
+		}
 	};
+
+	if(params.caller === 'OpacityTester') console.log('motionGenerator:ret===', ret);
+
+	return ret;
 }
 
 export default motionGenerator;
